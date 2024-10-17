84bce5de695220354a90e9ae35038e8931b02c9e
wildanfajri1alfarabi <wildanfajri85@gmail.com>
2024-10-17 20:56:26 +0700

    feat: add labels & comment

--- nom-co-case/NomNomCo.java
+++ nom-co-case/NomNomCo.java
@@ -30,14 +30,16 @@ abstract class Confectionary {
 class Cupcake extends Confectionary {
     @Override
     void bake() {
-        System.out.println("Baking a Cupcake named " + name + " with " + softness + " softness and " + toppings.size() + " toppings.");
+        System.out.println("Baking a Cupcake named " + name + " with " 
+        + softness + " softness and " + toppings.size() + " toppings.");
     }
 }
 // Concrete classes for Tart
 class Tart extends Confectionary {
     @Override
     void bake() {
-        System.out.println("Baking a Tart named " + name + " with " + softness + " softness and " + toppings.size() + " toppings.");
+        System.out.println("Baking a Tart named " + name + " with " 
+        + softness + " softness and " + toppings.size() + " toppings.");
     }
 }
 
@@ -84,6 +86,7 @@ class TransferAdapter implements PaymentProcessor {
         System.out.println("Price: $" + String.format("%.2f", amount) + " with Account: " + accountNumber);
     }
 
+    // GENERATE RANDOM ACCOUNT NUMBER WITH LENGTH 10
     private String generateAccountNumber() {
         SecureRandom random = new SecureRandom();
         StringBuilder accountNumber = new StringBuilder();
@@ -103,22 +106,24 @@ class CryptoAdapter implements PaymentProcessor {
         System.out.println("Price: " + cryptoCurrency + " " + String.format("%.2f", amount) + " with Address: " + cryptoAddress);
     }
 
+    // GENERATE RANDOM CRYPTO ADRESS WITH PREFIX "0x" with length 10
     private String generateCryptoAddress() {
         SecureRandom random = new SecureRandom();
-        StringBuilder cryptoAddress = new StringBuilder("0x");
+        StringBuilder cryptoAddress = new StringBuilder("0x"); // PREFIX 0x WITH STRING BUILDER
         String characters = "abcdefghijklmnopqrstuvwxyz0123456789";
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) { // PROSES LOOP SEBANYAK 10x
             int index = random.nextInt(characters.length());
             cryptoAddress.append(characters.charAt(index));
         }
-        return cryptoAddress.toString();
+        return cryptoAddress.toString(); // HASIL ADDRESS RANDOM WITH PREFIX "0x"
     }
 
+    // GENERATE RANDOM CRYPTO CURRENCY WITH LENGTH 3
     private String generateCryptoCurrency() {
         SecureRandom random = new SecureRandom();
         StringBuilder cryptoCurrency = new StringBuilder("");
         String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++) { 
             int index = random.nextInt(characters.length());
             cryptoCurrency.append(characters.charAt(index));
         }
@@ -138,6 +143,7 @@ class ConfectionaryDatabase {
         // Private constructor to prevent instantiation
     }
 
+    // FUNGSI INTI SINGLETON & INSISIASI OBJECT SINGLETON
     public static ConfectionaryDatabase getInstance() {
         if (instance == null) {
             instance = new ConfectionaryDatabase();
@@ -154,14 +160,14 @@ class ConfectionaryDatabase {
       if (confectionaryList.isEmpty()) {
           System.out.println("No Confectionary Yet...");
       } else {
-          for (Confectionary c : confectionaryList) {
+          for (Confectionary c : confectionaryList) { // SHORTHAND FUNCTION UNTUK LOOPING LIST OBJECT
               System.out.println("==========================");
               System.out.println("Name: " + c.name);
               System.out.println("Softness: " + c.softness);
 
               // Display toppings or a default message if no toppings are present
               if (c.toppings.isEmpty()) {
-                  System.out.println("Topping: -");
+                  System.out.println("Topping: No Toppings Added to This Confectionary");
               } else {
                   System.out.print("Topping: ");
                   for (int i = 0; i < c.toppings.size(); i++) {
@@ -243,22 +249,42 @@ class ConfectionaryDatabase {
 // TODO: #REGION =========== MAIN FUNCTION ==============
 public class NomNomCo {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        ConfectionaryDatabase database = ConfectionaryDatabase.getInstance();
+        Scanner scanner = new Scanner(System.in); // CLASS UNTUK GENERATE SCANNER INPUT PROMPT
+        ConfectionaryDatabase database = ConfectionaryDatabase.getInstance(); // PANGGIL DATABASE SINGLETON
+        // =============== CONTOH SINGLETON DAN PENGARUHNYA =======================
+        // ConfectionaryDatabase databaseB = ConfectionaryDatabase.getInstance(); // PANGGIL DATABASE SINGLETON
+        // ConfectionaryDatabase databaseC = ConfectionaryDatabase.getInstance(); // PANGGIL DATABASE SINGLETON
+        // database == databaseB == databaseC
+        // database ==> terletak di memory 0x123456
+        // databaseB ==> terletak di memory 0x123456
+        // databaseC ==> terletak di memory 0x123456
+
+        // =============== CONTOH BUKAN SINGLETON DAN PENGARUHNYA =======================
+        // ConfectionaryDatabase database1 = ConfectionaryDatabase(); // BUKAN SINGLETON
+        // ConfectionaryDatabase database2 = ConfectionaryDatabase(); // BUKAN SINGLETON
+        // ConfectionaryDatabase database3 = ConfectionaryDatabase(); // BUKAN SINGLETON
+        // database1 != database2 != database3
+        // database1 ==> terletak di memory 0xabcde
+        // database2 ==> terletak di memory 0xbcdahg
+        // database2 ==> terletak di memory 0x24aefb
+
         String choice;
 
         do {
+            // MAIN MENU UI PROMPT
             System.out.println("Nom Nom Co.");
             System.out.println("===========");
             System.out.println("1. Bake Confectionary");
             System.out.println("2. View Confectionary Order");
             System.out.println("3. Exit");
             System.out.print(">> ");
+            
+            // INSERT PROMPT UNTUK MEMILIH MENU
             choice = scanner.nextLine();
 
             switch (choice) {
                 case "1":
-                    bakeConfectionary(scanner, database);
+                    bakeConfectionary(scanner, database); // BUTUH 2 ARGUMEN, SCANNER PROMPT DAN DATABASE
                     break;
                 case "2":
                     database.viewConfectionaryOrders();
@@ -267,37 +293,37 @@ public class NomNomCo {
                     System.out.println("Exiting...");
                     break;
                 default:
-                    System.out.println("Invalid option. Try again.");
+                    System.out.println("Invalid optionm, please choose between menu [1, 2 or 3]. Try again.");
             }
-        } while (!choice.equals("3"));
+        } while (!choice.equals("3")); // KALO PILIHANNYA MENU 3, AKAN KELUAR LOOP
 
         scanner.close();
     }
 
     public static void bakeConfectionary(Scanner scanner, ConfectionaryDatabase database) {
-        ConfectionaryFactory factory = null;
+        ConfectionaryFactory factory = null; // PENGGUNAAN CONFECTIONARY FACTORY
 
         // Confectionary Type
         String type;
         do {
             System.out.print("Enter confectionary type (Cupcake/Tart): ");
             type = scanner.nextLine();
-        } while (!type.equals("Cupcake") && !type.equals("Tart"));
+        } while (!type.equals("Cupcake") && !type.equals("Tart")); // LOOP AKAN BERHENTI APABILA TIPENYA ADALAH Cupcake/Tart
 
         if (type.equals("Cupcake")) {
-            factory = new CupcakeFactory();
+            factory = new CupcakeFactory(); // BUKAN BUAT CONCRETE CUPCAKE TAPI FACTORY CUPCAKE
         } else if (type.equals("Tart")) {
-            factory = new TartFactory();
+            factory = new TartFactory(); // BUKAN BUAT CONCRETE TART TAPI FACTORY TART
         }
 
-        Confectionary confectionary = factory.createConfectionary();
+        Confectionary confectionary = factory.createConfectionary(); // BUAT CONFECTIONARY BERDASARKAN FACTORY YG DIPILIH SEBELUMNYA
 
         // Confectionary Name
         String name;
         do {
             System.out.print("Enter confectionary name (5-15 chars): ");
             name = scanner.nextLine();
-        } while (name.length() < 5 || name.length() > 15);
+        } while (name.length() < 5 || name.length() > 15); // VALIDASI NAMA HARUS [5-15]
         confectionary.name = name;
 
         // Confectionary Softness
@@ -305,7 +331,7 @@ public class NomNomCo {
         do {
             System.out.print("Enter confectionary softness (Fluffy/Medium/Hard): ");
             softness = scanner.nextLine();
-        } while (!softness.equals("Fluffy") && !softness.equals("Medium") && !softness.equals("Hard"));
+        } while (!softness.equals("Fluffy") && !softness.equals("Medium") && !softness.equals("Hard")); // VALIDASI TIPE KELEMBUTAN HARUS FLUFFY/MEDIUM/HARD
         confectionary.softness = softness;
 
         // Toppings
@@ -316,13 +342,13 @@ public class NomNomCo {
         } while (!toppingOption.equals("Y") && !toppingOption.equals("N"));
 
         if (toppingOption.equals("Y")) {
-            for (int i = 0; i < 3; i++) {
+            for (int i = 0; i < 3; i++) { // ISI PROMPT TOPPING SEBANYAK 3x
                 String topping;
                 do {
                     System.out.print("Enter topping " + (i + 1) + " (1-10 chars): ");
                     topping = scanner.nextLine();
-                } while (topping.length() < 1 || topping.length() > 10);
-                confectionary.toppings.add(topping);
+                } while (topping.length() < 1 || topping.length() > 10); // VALIDASI NAMA HARUS SEPANJANG [1-10]
+                confectionary.toppings.add(topping); // INSERT TOPPING KE CONFECTIONARY
             }
         }
 
@@ -330,8 +356,8 @@ public class NomNomCo {
         double price;
         do {
             System.out.print("Enter confectionary price (10.0 - 50.0): ");
-            price = Double.parseDouble(scanner.nextLine());
-        } while (price < 10.0 || price > 50.0);
+            price = Double.parseDouble(scanner.nextLine()); // CONVERT HARGA DARI STRING KE DOUBLE
+        } while (price < 10.0 || price > 50.0); // VALIDASI HARGA HARUS DIANTARA [10.0 - 50.0]
         confectionary.price = price;
 
         // Payment Type
@@ -339,12 +365,14 @@ public class NomNomCo {
         do {
             System.out.print("Enter payment type (Cash/Transfer/Crypto): ");
             paymentType = scanner.nextLine();
+            // VALIDASI PAYMENT HARUS ANTARA CASH/TRANSFER/CRYPTO
         } while (!paymentType.equals("Cash") && !paymentType.equals("Transfer") && !paymentType.equals("Crypto"));
         confectionary.paymentType = paymentType;
 
         // Convert price based on payment type
         switch (paymentType) {
             case "Cash":
+                confectionary.price = price * 1;
                 break;
             case "Transfer":
                 confectionary.price = price * 1.1;
