28c5a5100e75f76aa149359a8a1398c82aa6e91e
wildanfajri1alfarabi <wildanfajri85@gmail.com>
2024-11-25 19:07:46 +0700

    feat: modify previous chain responsibility usecase

--- chain-responsibility-pattern-case/CustomerSupportSystem.java
+++ chain-responsibility-pattern-case/CustomerSupportSystem.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
+
 enum IssueType {
     ACCOUNT_HELP,
-    REFUND,
-    TECHNICAL
+    REFUND, 
+    TECHNICAL,
+    // TAMBAH ISSUE TYPE
+    COMPLAINT,
+    HACKED,
+    NOT_AVAILABLE
 }
 
-// Currency kalo di use-case sebelumnya
 class SupportTicket {
     private final IssueType issueType;
     private final String description;
@@ -23,11 +28,9 @@ class SupportTicket {
     }
 }
 
-// Interface/Abstract Chain Handler
 abstract class SupportHandler {
     private SupportHandler nextHandler;
 
-    //#region Concrete Function
     public void setNextHandler(SupportHandler nextHandler) {
         this.nextHandler = nextHandler;
     }
@@ -41,19 +44,13 @@ abstract class SupportHandler {
             System.out.println("No support available for this issue: " + ticket.getDescription());
         }
     }
-    //#endregion
 
-    //#region Abstract Function (Need to be overided)
     protected abstract boolean canHandle(SupportTicket ticket);
 
     protected abstract void processTicket(SupportTicket ticket);
-    //#endregion
 }
 
-//#region Concrete/Child Chain Handler
 class Level1Support extends SupportHandler {
-    //#region Overrided Abstract Function 
-    // LEVEL-1 SUPPORT CAN ONLY HANDLE ISSUE TYPE ACCOUNT HELP
     @Override
     protected boolean canHandle(SupportTicket ticket) {
         return ticket.getIssueType() == IssueType.ACCOUNT_HELP;
@@ -63,67 +60,100 @@ class Level1Support extends SupportHandler {
     protected void processTicket(SupportTicket ticket) {
         System.out.println("Level 1 Support: Resolving account issue - " + ticket.getDescription());
     }
-    //#endregion
-  }
-  
+}
+
 class Level2Support extends SupportHandler {
-    //#region Overrided Abstract Function 
-    // LEVEL-2 SUPPORT CAN ONLY HANDLE ISSUE TYPE REFUND
     @Override
     protected boolean canHandle(SupportTicket ticket) {
-        return ticket.getIssueType() == IssueType.REFUND;
+        return ticket.getIssueType() == IssueType.REFUND || ticket.getIssueType() == IssueType.COMPLAINT;
     }
-    
+
     @Override
     protected void processTicket(SupportTicket ticket) {
-        System.out.println("Level 2 Support: Resolving refund issue - " + ticket.getDescription());
+        System.out.println("Level 2 Support: Resolving refund/complaint issue - " + ticket.getDescription());
     }
-    //#endregion
 }
-  
+
 class Level3Support extends SupportHandler {
-    //#region Overrided Abstract Function 
-    // LEVEL-3 SUPPORT CAN ONLY HANDLE ISSUE TYPE TECHNICAL
     @Override
     protected boolean canHandle(SupportTicket ticket) {
         return ticket.getIssueType() == IssueType.TECHNICAL;
     }
-  
+
     @Override
     protected void processTicket(SupportTicket ticket) {
         System.out.println("Level 3 Support: Resolving technical issue - " + ticket.getDescription());
     }
-    //#endregion
-} 
-//#endregion 
-  
-//#region MAIN APPLICATION
+}
+
+class Level4Support extends SupportHandler {
+    @Override
+    protected boolean canHandle(SupportTicket ticket) {
+        return ticket.getIssueType() == IssueType.HACKED;
+    }
+
+    @Override
+    protected void processTicket(SupportTicket ticket) {
+        System.out.println("Level 4 Support: Resolving hacked issue - " + ticket.getDescription());
+    }
+}
+
 public class CustomerSupportSystem {
+    public static IssueType getIssueType(String serviceType) {
+        switch (serviceType.toLowerCase()) {
+            case "account":
+                return IssueType.ACCOUNT_HELP;
+            case "refund":
+                return IssueType.REFUND;
+            case "complaint":
+                return IssueType.COMPLAINT;
+            case "technical":
+                return IssueType.TECHNICAL;
+            case "hacked":
+                return IssueType.HACKED;
+            default:
+                return IssueType.NOT_AVAILABLE;
+        }
+    }
+
     public static void main(String[] args) {
-        // Create Handlers
         SupportHandler level1 = new Level1Support();
         SupportHandler level2 = new Level2Support();
         SupportHandler level3 = new Level3Support();
+        SupportHandler level4 = new Level4Support();
 
-        // Chain Handlers
         level1.setNextHandler(level2);
         level2.setNextHandler(level3);
+        level3.setNextHandler(level4);
 
-        // Create Tickets
-        // Tiket 1 untuk issue Account Help dengan deskripsi "lupa password"
-        SupportTicket ticket1 = new SupportTicket(IssueType.ACCOUNT_HELP, "Forgot password");
-        // Tiket 2 untuk issue Refund dengan deskripsi "meminta refund untuk transaksi dengan id #12345"
-        SupportTicket ticket2 = new SupportTicket(IssueType.REFUND, "Requesting refund for order #12345");
-        // Tiket 3 untuk issue Technical dengan deskripsi "website tidak bisa loading"
-        SupportTicket ticket3 = new SupportTicket(IssueType.TECHNICAL, "Website not loading properly");
-        // Tiket 4 untuk issue Technical dengan deskripsi "website crash ketika login"
-        SupportTicket ticket4 = new SupportTicket(IssueType.TECHNICAL, "App crashing on login");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("=== Ticketing System ===");
+        System.out.println("Do you want to create a new ticket? (1: yes, 0: no): ");
+        int choice = scanner.nextInt();
+        scanner.nextLine();
 
-        // Process Tickets
-        System.out.println("Processing Support Tickets:");
-        level1.handleTicket(ticket1);
-        level1.handleTicket(ticket2);
-        level1.handleTicket(ticket3);
-        level1.handleTicket(ticket4);
+        while (choice == 1) {
+            System.out.print("Enter the issue type (Account, Refund, Complaint, Technical, Hacked): ");
+            String serviceType = scanner.nextLine();
+            IssueType issueType = getIssueType(serviceType);
+
+            if (issueType == IssueType.NOT_AVAILABLE) {
+                System.out.println("Please insert a valid issue type!");
+            } else {
+                System.out.print("Enter ticket description: ");
+                String desc = scanner.nextLine();
+
+                SupportTicket ticket = new SupportTicket(issueType, desc);
+                
+                level1.handleTicket(ticket);
+            }
+
+            System.out.println("Do you want to create a new ticket? (1: yes, 0: no): ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+        }
+
+        System.out.println("Exiting Ticketing System. Goodbye!");
+        scanner.close();
     }
 }
